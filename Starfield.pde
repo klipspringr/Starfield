56885e51edc015481832f167948011346041ee62
unknown <maibarra@697-334-Gimli.sf.k12.internal>
2014-10-22 09:59:20 -0700

    updated project to have jumbo

--- Starfield.pde
+++ Starfield.pde
@@ -1,9 +1,10 @@
 Particle[] manyBobs;
 void setup()
 {
+	background(0);
 	size(800,800);
-	// frameRate(10);
-	manyBobs = new Particle[200];
+	frameRate(60);
+	manyBobs = new Particle[250];
 	for (int i = 0; i < manyBobs.length; i++)
  	{
  		manyBobs[0] = new OddballParticle();
@@ -13,24 +14,26 @@ void setup()
 }
 void draw()
 {
-	background(0);
+	fill(0,10);
+	rect(0, 0, 800, 805);	
 	for (int i = 0; i < manyBobs.length; i++)
 	{
 		manyBobs[i].move();
+		manyBobs[i].wrap();
 		manyBobs[i].show();
 	}
 	
 }
 class NormalParticle implements Particle
 {
-	int myColor, redColor, greenColor, blueColor, myAngle;
-	double myX, myY, mySpeed;
+	int myColor, redColor, greenColor, blueColor;
+	double myX, myY, mySpeed, myAngle;
 	NormalParticle()
 	{
 		myX = 400;
 		myY = 400;
-		myAngle = (int)(Math.random()*361);
-		mySpeed = (int)(Math.random()*3);
+		myAngle = (Math.random()*2*Math.PI);
+		mySpeed = (int)(Math.random()*10);
 		redColor = (int)(Math.random()*256);
 		greenColor = (int)(Math.random()*256);
 		blueColor = (int)(Math.random()*256);
@@ -46,19 +49,28 @@ class NormalParticle implements Particle
 	{
 		stroke(myColor);
 		fill(myColor);
-		ellipse((float)myX, (float)myY, 4, 4);
+		ellipse((float)myX, (float)myY, 10, 10);
+	}
+	public void wrap()
+	{
+		if(myX >= 800 || myY >= 800 || myX <= 0 || myY <=0)
+	    {
+	    	myX = 400;
+	    	myY = 400;
+	    	myAngle = (Math.random()*2*Math.PI);
+	    }
 	}
 }
-class JumboParticle implements Particle
+class JumboParticle extends NormalParticle
 {
-	int myColor, redColor, greenColor, blueColor, myAngle;
-	double myX, myY, mySpeed;
+	int myColor, redColor, greenColor, blueColor;
+	double myX, myY, mySpeed, myAngle;
 	JumboParticle()
 	{
 		myX = 400;
 		myY = 400;
-		myAngle = (int)(Math.random()*361);
-		mySpeed = (int)(Math.random()*3);
+		myAngle = (Math.random()*2*Math.PI);
+		mySpeed = (int)(Math.random()*6);
 		redColor = (int)(Math.random()*256);
 		greenColor = (int)(Math.random()*256);
 		blueColor = (int)(Math.random()*256);
@@ -74,13 +86,23 @@ class JumboParticle implements Particle
 	{
 		stroke(myColor);
 		fill(myColor);
-		ellipse((float)myX, (float)myY, 20, 20);
+		rect((float)myX, (float)myY, 20, 20);
 	}
+	public void wrap()
+	{
+		if(myX >= 800 || myY >= 800 || myX <= 0 || myY <=0)
+	    {
+	    	myX = 400;
+	    	myY = 400;
+	    }
+	}
+
 }
 interface Particle
 {
 	public void show();
 	public void move();
+	public void wrap();
 }
 class OddballParticle implements Particle
 {
@@ -90,8 +112,8 @@ class OddballParticle implements Particle
 	{
 		myX = 400;
 		myY = 400;
-		myAngle = (double)(Math.random()*361);
-		mySpeed = (int)(Math.random()*1);
+		myAngle = (Math.random()*2*Math.PI);
+		mySpeed = (int)(Math.random()*10);
 		redColor = 100;
 		greenColor = 22;
 		blueColor =22;
@@ -103,7 +125,10 @@ class OddballParticle implements Particle
 	{
 		myX = (Math.cos(myAngle)* mySpeed) + myX;
 		myY = Math.sin(myAngle) + myY;
-		mySize ++;
+		if (mySize <= 100)
+		{
+			mySize ++;
+		}
 		
 	}
 	public void show()
@@ -111,6 +136,14 @@ class OddballParticle implements Particle
 		stroke(myColor);
 		fill(myColor);
 		ellipse((float)myX, (float)myY, mySize, mySize);
+	}
+	public void wrap()
+	{
+		if(myX >= 800 || myY >= 800 || myX <= 0 || myY <=0)
+	    {
+	    	myX = 400;
+	    	myY = 400;
+	    }
 	}
 }
 
--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -17,9 +17,10 @@ public class Starfield extends PApplet {
 Particle[] manyBobs;
 public void setup()
 {
+	background(0);
 	size(800,800);
-	// frameRate(10);
-	manyBobs = new Particle[200];
+	frameRate(60);
+	manyBobs = new Particle[250];
 	for (int i = 0; i < manyBobs.length; i++)
  	{
  		manyBobs[0] = new OddballParticle();
@@ -29,24 +30,26 @@ public void setup()
 }
 public void draw()
 {
-	background(0);
+	fill(0,10);
+	rect(0, 0, 800, 805);	
 	for (int i = 0; i < manyBobs.length; i++)
 	{
 		manyBobs[i].move();
+		manyBobs[i].wrap();
 		manyBobs[i].show();
 	}
 	
 }
 class NormalParticle implements Particle
 {
-	int myColor, redColor, greenColor, blueColor, myAngle;
-	double myX, myY, mySpeed;
+	int myColor, redColor, greenColor, blueColor;
+	double myX, myY, mySpeed, myAngle;
 	NormalParticle()
 	{
 		myX = 400;
 		myY = 400;
-		myAngle = (int)(Math.random()*361);
-		mySpeed = (int)(Math.random()*3);
+		myAngle = (Math.random()*2*Math.PI);
+		mySpeed = (int)(Math.random()*10);
 		redColor = (int)(Math.random()*256);
 		greenColor = (int)(Math.random()*256);
 		blueColor = (int)(Math.random()*256);
@@ -62,19 +65,28 @@ class NormalParticle implements Particle
 	{
 		stroke(myColor);
 		fill(myColor);
-		ellipse((float)myX, (float)myY, 4, 4);
+		ellipse((float)myX, (float)myY, 10, 10);
+	}
+	public void wrap()
+	{
+		if(myX >= 800 || myY >= 800 || myX <= 0 || myY <=0)
+	    {
+	    	myX = 400;
+	    	myY = 400;
+	    	myAngle = (Math.random()*2*Math.PI);
+	    }
 	}
 }
-class JumboParticle implements Particle
+class JumboParticle extends NormalParticle
 {
-	int myColor, redColor, greenColor, blueColor, myAngle;
-	double myX, myY, mySpeed;
+	int myColor, redColor, greenColor, blueColor;
+	double myX, myY, mySpeed, myAngle;
 	JumboParticle()
 	{
 		myX = 400;
 		myY = 400;
-		myAngle = (int)(Math.random()*361);
-		mySpeed = (int)(Math.random()*3);
+		myAngle = (Math.random()*2*Math.PI);
+		mySpeed = (int)(Math.random()*6);
 		redColor = (int)(Math.random()*256);
 		greenColor = (int)(Math.random()*256);
 		blueColor = (int)(Math.random()*256);
@@ -90,13 +102,23 @@ class JumboParticle implements Particle
 	{
 		stroke(myColor);
 		fill(myColor);
-		ellipse((float)myX, (float)myY, 20, 20);
+		rect((float)myX, (float)myY, 20, 20);
 	}
+	public void wrap()
+	{
+		if(myX >= 800 || myY >= 800 || myX <= 0 || myY <=0)
+	    {
+	    	myX = 400;
+	    	myY = 400;
+	    }
+	}
+
 }
 interface Particle
 {
 	public void show();
 	public void move();
+	public void wrap();
 }
 class OddballParticle implements Particle
 {
@@ -106,8 +128,8 @@ class OddballParticle implements Particle
 	{
 		myX = 400;
 		myY = 400;
-		myAngle = (double)(Math.random()*361);
-		mySpeed = (int)(Math.random()*1);
+		myAngle = (Math.random()*2*Math.PI);
+		mySpeed = (int)(Math.random()*10);
 		redColor = 100;
 		greenColor = 22;
 		blueColor =22;
@@ -119,7 +141,10 @@ class OddballParticle implements Particle
 	{
 		myX = (Math.cos(myAngle)* mySpeed) + myX;
 		myY = Math.sin(myAngle) + myY;
-		mySize ++;
+		if (mySize <= 100)
+		{
+			mySize ++;
+		}
 		
 	}
 	public void show()
@@ -127,6 +152,14 @@ class OddballParticle implements Particle
 		stroke(myColor);
 		fill(myColor);
 		ellipse((float)myX, (float)myY, mySize, mySize);
+	}
+	public void wrap()
+	{
+		if(myX >= 800 || myY >= 800 || myX <= 0 || myY <=0)
+	    {
+	    	myX = 400;
+	    	myY = 400;
+	    }
 	}
 }
 
